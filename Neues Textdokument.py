dd8c8081dd6f858b3fadab77cd52db54955f764d
Dieter Holstein <holstein.dieter@web.de>
2023-12-09 17:32:45 +0000

    java

--- Neues Textdokument.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Unsicheres Code-Snippet, das ein Command-Injection-Sicherheitsrisiko enthält
-user_input = input("Bitte geben Sie einen Dateinamen ein: ")
-os.system(f"cat {user_input}")
