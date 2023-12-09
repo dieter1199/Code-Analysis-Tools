336c6d4792ebccfb04786419e2207721d52c6f6c
Dieter Holstein <holstein.dieter@web.de>
2023-12-09 15:02:13 +0000

    Clean

--- Vulnerable_code.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# Beispiel für unsicheren Code
-def unsichere_subprocess_nutzung(user_input):
-    subprocess.call("echo " + user_input, shell=True)
-
-# Beispielhafter Aufruf
-unsichere_subprocess_nutzung("Hello World")
